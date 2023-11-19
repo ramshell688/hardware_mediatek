88f836bb025d945f2a88def2264f847dbe5bf244
rams688 <ramonzost68@gmail.com>
2023-11-19 11:39:25 +0700

    camellia: Move to WLAN folder
    Signed-off-by: rams688 <ramonzost68@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
 ifeq ($(BOARD_HAS_MTK_HARDWARE),true)
 include $(call all-subdir-makefiles)
-else ifeq ($(BOARD_WLAN_DEVICE), MediaTek)
-include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
---
+++ wlan/Android.mk
@@ -0,0 +1,3 @@
+ifeq ($(BOARD_WLAN_DEVICE), MediaTek)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
